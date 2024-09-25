75680dd0c6355667fb5185cee8d871e33b33fc7f
sandesh300 <bhujbalsandesh52@gmail.com>
2024-09-25 23:17:24 +0530

    add swaps numbers using method

---
+++ Java-Program-Important/SwapNumbersUsingMethod.java
@@ -0,0 +1,21 @@
+//swaps two numbers using method
+
+public class SwapNumbersUsingMethod
+{
+    public static void swap(int a, int b)
+    {
+        a = (a & b) + (a | b);  //a=a+b
+
+        b = a + (~b) + 1;  //b=a-b
+        
+        a = a + (~b) + 1;  //a=a-b;
+
+        System.out.println("After swapping : a= "+a+ "b= "+b);
+    }
+    public static void main(String[] args)
+    {
+    int a = 10, b = 200;
+    swap(a, b);
+
+    }
+}
