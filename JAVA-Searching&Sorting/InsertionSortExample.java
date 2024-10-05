929530358a1b71fa838e07cba0dd200ddca09297
sandesh300 <bhujbalsandesh52@gmail.com>
2024-10-05 22:05:30 +0530

    update the code

--- JAVA-Searching&Sorting/InsertionSortExample.java
+++ JAVA-Searching&Sorting/InsertionSortExample.java
@@ -1,40 +1,33 @@
 
 public class InsertionSortExample {
-public static void InsertionSort(int arr[])
-{
-	for(int j=1; j<arr.length; j++)
-	{
-		int key=arr[j];
-		
-		int i=j-1;
-		while((i>-1) && (arr[i]>key))
-		{
-			arr[i+1]=arr[i];
-			i--;
+	public static void insertionSort(int arr[]) {
+		for (int i = 0; i <= arr.length - 1; i++) {
+			int j = i;
+			while (j > 0 && arr[j - 1] > arr[j]) {
+				int temp = arr[j - 1];
+				arr[j - 1] = arr[j];
+				arr[j] = temp;
+				j--;
+			}
 		}
-		arr[i+1]=key;
 	}
-}
-	
-  public static void main(String[] args) {
-	
-	  int arr1[]= {9,14,3,2,43,11,58,22};
-	  
-	  System.out.println("Before insertion sort");
-	  for(int i=0; i<arr1.length; i++)
-	  {
-		  System.out.print(arr1[i]+"  ");
-	  }
-	  System.out.println();
-	  
-	  
-	  InsertionSort(arr1);
-	  System.out.println("After insertion sort");
-	  for(int i=0; i<arr1.length; i++)
-	  {
-		  System.out.print(arr1[i]+"  ");
-	  }
-	  
-}
-	
+
+	public static void main(String[] args) {
+
+		int arr1[] = { 9, 14, 3, 2, 43, 11, 58, 22 };
+
+		System.out.println("Before insertion sort");
+		for (int i = 0; i < arr1.length; i++) {
+			System.out.print(arr1[i] + "  ");
+		}
+		System.out.println();
+
+		insertionSort(arr1);
+		System.out.println("After insertion sort");
+		for (int i = 0; i < arr1.length; i++) {
+			System.out.print(arr1[i] + "  ");
+		}
+
+	}
+
 }
