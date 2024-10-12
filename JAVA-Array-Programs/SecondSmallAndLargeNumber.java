fd2bbe1f5fff40251fa220fd41ba748986735bd2
sandesh300 <bhujbalsandesh52@gmail.com>
2024-10-12 21:43:02 +0530

    add SecondSmallAndLargeNumber code

---
+++ JAVA-Array-Programs/SecondSmallAndLargeNumber.java
@@ -0,0 +1,39 @@
+// Find Second Smallest and Second Largest Element in an array
+
+public class SecondSmallAndLargeNumber {
+
+    static int secondLargestNum(int arr[]) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr[arr.length - 2];
+    }
+
+    static int secondSmallestNum(int arr[]) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr[1];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 6, 3, 5 };
+        System.out.println("Second Largest number is = " + secondLargestNum(arr));
+        System.out.println("Second Smallest number is = " + secondSmallestNum(arr));
+
+    }
+}
