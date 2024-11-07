530df35ae5fb74e180e24ccf66a9064fabca7a88
sandesh300 <bhujbalsandesh52@gmail.com>
2024-11-07 21:53:49 +0530

    add code

--- Java-Program-Important/GCD.java
+++ Java-Program-Important/GCD.java
@@ -1,21 +1,18 @@
-
+// 
 public class GCD {
-public static void main(String[] args) {
-	
-	int n1= 5021956;
-	int n2= 2991019;
-	
-	while(n1!=n2)
-	{
-		if(n1>n2)
-		
-			n1=n1-n2;
-		
-		else
-		
-			n2=n2-n1;
-		
+	public static void main(String[] args) {
+		int n1 = 10;
+		int n2 = 15;
+
+		while (n1 != n2) {
+			if (n1 > n2) {
+				n1 = n1 - n2;
+			} else {
+				n2 = n2 - n1;
+			}
+
+		}
+		System.out.println(n2);
 	}
-	System.out.println("the GCD is== "+n2);
-}
+
 }
