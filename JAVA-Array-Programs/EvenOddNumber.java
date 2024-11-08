cefe85bea0a7503b5db0db37e04c2920adb7d807
sandesh300 <bhujbalsandesh52@gmail.com>
2024-11-08 12:28:33 +0530

    add the java 8 lambda expression code

--- JAVA-Array-Programs/EvenOddNumber.java
+++ JAVA-Array-Programs/EvenOddNumber.java
@@ -2,25 +2,25 @@
 public class EvenOddNumber {
 public static void main(String[] args) {
 	
-	int a[]= {1,2,3,4,5,6};
+	int arr[]= {1,2,3,4,5,6};
 	
 	System.out.println("Even Numbers = ");
-	for(int i=0; i<a.length; i++)
+	for(int i=0; i<arr.length; i++)
 	{
-		if(a[i] % 2 == 0)
+		if(arr[i] % 2 == 0)
 		{
-			System.out.println(a[i]);     // Even Numbers = 2, 4, 6
+			System.out.println(arr[i]);     // Even Numbers = 2, 4, 6
 		}
 	}
 
 
 	
 	System.out.println("Odd Numbers = ");
-	for(int i=0; i<a.length; i++)
+	for(int i=0; i<arr.length; i++)
 	{
-		if(a[i] % 2 != 0)
+		if(arr[i] % 2 != 0)
 		{
-			System.out.println(a[i]);    // Odd Numbers = 1, 3, 5
+			System.out.println(arr[i]);    // Odd Numbers = 1, 3, 5
 		}
 	}	
 
