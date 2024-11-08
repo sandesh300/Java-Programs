6b48d7b59e20241fbf7af10c8fc3d45a4ad4365b
sandesh300 <bhujbalsandesh52@gmail.com>
2024-11-08 12:20:11 +0530

    update the code

--- JAVA-Array-Programs/CopyElement.java
+++ JAVA-Array-Programs/CopyElement.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
+// Program to Copy elements form one array to other array.
 
 public class CopyElement {
 
 	public static void main(String[] args) {
 
-		int arr1[] = new int[] { 11, 22, 33, 44, 55 }; // creating one array
+		int arr1[] = { 11, 22, 33, 44, 55 }; // creating one array
 		int arr2[] = new int[arr1.length]; // creating 2nd array with the size of 1st array
 
 		for (int i = 0; i < arr1.length; i++) // copying one array into another
@@ -25,16 +25,16 @@ public class CopyElement {
 		// ------------------------------------------------------------------------------------------------------------------------------------
 		// Copying char array elements from oArray[ ] into nArray[] using clone method.
 
-		char[] oArray = new char[] { 'S', 'A', 'N', 'D', 'E', 'S', 'H' };
-		char[] nArray = oArray.clone();
+		char arr1[] = new char[] { 'S', 'A', 'N', 'D', 'E', 'S', 'H' };
+		char arr2[] = arr1.clone();
 
-		System.out.println("Elements of oArray[]: ");
-		for (int i = 0; i < oArray.length; i++)
-			System.out.print(oArray[i] + " ");
+		System.out.println("Elements of []: ");
+		for (int i = 0; i < arr1.length; i++)
+			System.out.print(arr1[i] + " ");
 
 		System.out.println("\n\nElements of nArray[]: ");
-		for (int i = 0; i < nArray.length; i++)
-			System.out.print(nArray[i] + " ");
+		for (int i = 0; i < arr2.length; i++)
+			System.out.print(arr2[i] + " ");
 
 	}
 }
