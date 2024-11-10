e755a60e2caac97f4220b4193a6bfd1ed5c89526
sandesh300 <bhujbalsandesh52@gmail.com>
2024-11-10 21:20:41 +0530

    add java 8 predicate code

--- JAVA-Searching&Sorting/BubbleSortExample.java
+++ JAVA-Searching&Sorting/BubbleSortExample.java
@@ -2,39 +2,37 @@
 
 public class BubbleSortExample {
 
-	public static void BubbleSort(int arr[])
-	{
-		int temp=0;
-		for(int i=0; i<arr.length; i++)
-		{
-			for(int j=1; j<(arr.length-i); j++)
-			{
-				if(arr[j-1]>arr[j])
-				{
-					temp=arr[j-1];
-					arr[j-1]=arr[j];
-					arr[j]=temp;
+	public static void BubbleSort(int arr[]) {
+		int temp = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 1; j < (arr.length - i); j++) {
+				if (arr[j - 1] > arr[j]) {
+					temp = arr[j - 1];
+					arr[j - 1] = arr[j];
+					arr[j] = temp;
 				}
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		int arr[]= {30,15,5,2,99,60,70,45};
-		
+		int arr[] = { 30, 15, 5, 2, 99, 60, 70, 45 };
+
 		System.out.println("Array before bubble sort= ");
-		for(int i=0; i<arr.length; i++)
-		{
-			System.out.print(arr[i]+" ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
 		}
 		System.out.println();
-	
-	BubbleSort(arr);
-	System.out.println("Array after bubble sort= ");
-	for(int i=0; i<arr.length; i++)
-	{
-		System.out.print(arr[i]+" ");
+
+		BubbleSort(arr);
+		System.out.println("Array after bubble sort= ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
 	}
 }
-}
 
+// Array before bubble sort= 
+// 30 15 5 2 99 60 70 45 
+// Array after bubble sort= 
+// 2 5 15 30 45 60 70 99 
