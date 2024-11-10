ebde625118116b9149f9dbc33ad6f9e286b3a21e
sandesh300 <bhujbalsandesh52@gmail.com>
2024-11-10 20:48:23 +0530

    add code in java 8

--- JAVA-Searching&Sorting/LinearSearchExample.java
+++ JAVA-Searching&Sorting/LinearSearchExample.java
@@ -2,25 +2,24 @@
 
 public class LinearSearchExample {
 
-	public static int LinearSearch(int arr[], int key)
-	{
-		for(int i=0; i<arr.length; i++)
-		{
-			if(arr[i]==key)
-			{
+	public static int LinearSearch(int arr[], int key) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == key) {
 				return i;
-		}
+			}
 		}
 		return -1;
 	}
-	public static void main(String[] args) 
-	{
-	int a1[]= {10,20,30,40,50,60,70,80,90,100};
-	
-	int key=50;
-	
-	System.out.println(key+"is found at " +LinearSearch(a1, key));
-	
+
+	public static void main(String[] args) {
+		int a1[] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+		int key = 50;
+
+		System.out.println(key + " is found at " + LinearSearch(a1, key));
+
 	}
-	
+
 }
+
+// output - 50 is found at 4
