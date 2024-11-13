89e986c74bf6ae9e028a202d0bbfbc938121ffca
sandesh300 <bhujbalsandesh52@gmail.com>
2024-11-13 23:52:30 +0530

    predicate code java 8

--- java-8-Programs/predicate/Test1.java
+++ java-8-Programs/predicate/Test1.java
@@ -3,9 +3,12 @@ package predicate;
 import java.util.function.Predicate;
 
 public class Test1 {
-public static void main(String[] args) {
-	Predicate<Integer> p = I -> (I > 10);
-	System.out.println(p.test(100));
-	System.out.println(p.test(9));
+	public static void main(String[] args) {
+		Predicate<Integer> p = I -> (I > 10);
+		System.out.println(p.test(100));
+		System.out.println(p.test(9));
+	}
 }
-}
+
+// output - true
+//          false
