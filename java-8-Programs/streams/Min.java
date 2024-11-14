07e0a7ed86c303e956db946e8188baa3ff301c7f
sandesh300 <bhujbalsandesh52@gmail.com>
2024-11-14 15:55:45 +0530

    java 8 streams Min.java

--- java-8-Programs/streams/Min.java
+++ java-8-Programs/streams/Min.java
@@ -3,8 +3,8 @@ package streams;
 import java.util.*;
 
 public class Min {
-   public static void main(String[] args) {
-	
+	public static void main(String[] args) {
+
 		ArrayList<Integer> arr = new ArrayList<>();
 		arr.add(10);
 		arr.add(15);
@@ -13,11 +13,14 @@ public class Min {
 		arr.add(30);
 		arr.add(40);
 		arr.add(50);
-		
+
 		System.out.println(arr);
-		
+
 		Integer min = arr.stream().min((i1, i2) -> i1.compareTo(i2)).get();
-		
+
 		System.out.println(min);
+	}
 }
-}
+
+// [10, 15, 25, 20, 30, 40, 50]
+// 10
