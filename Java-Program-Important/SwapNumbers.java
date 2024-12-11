8e354e9c6c622ac9566f514dbd1b3b6f95d88128
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-11 16:51:45 +0530

    swap numbers code

--- Java-Program-Important/SwapNumbers.java
+++ Java-Program-Important/SwapNumbers.java
@@ -1,26 +1,35 @@
+// Program to swap the numbers without using the third variable.
 
 public class SwapNumbers {
-public static void main(String[] args) {
-	
-	int x=10;
-	int y=200;
-	
-	  x = x + y;
-	  y = x - y;
-	  x= x + y;
-	
-	System.out.println("After swapping=\n  x= " +x+",\n  y= "+y);
+	public static void main(String[] args) {
+
+		int x = 10;
+		int y = 200;
+
+		System.out.println("Before swapping:  x= " + x + ",  y= " + y);  // Before swapping:  x= 10,  y= 200
+
+		x = x + y;
+		y = x - y;
+		x = x + y;
+
+		System.out.println("After swapping:  x= " + x + ",  y= " + y);  // After swapping:  x= 220,  y= 10
+	}
 }
-}
 
 
+/* 
+// swap numbers using third variable
 
-//swap number without using third variable
-//  int temp = x;
-//  x = y;
-//  y = temp;
+int temp = x;
+x = y;
+y = temp;
 
-//swap number using bitwise x-or operator 
-// x = x ^ y;
-// y = x ^ y;
-// x = x ^ y;
+
+// swap numbers without using third variable
+// swap number using bitwise x-or operator
+
+x = x ^ y;
+y = x ^ y;
+x = x ^ y;
+
+*/
