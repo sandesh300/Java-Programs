8e5401a7cad0ce8319ca201d6881428f73495324
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-11 13:54:41 +0530

    update the area of triangle code

--- Java-Program-Important/AreaOfTriangle.java
+++ Java-Program-Important/AreaOfTriangle.java
@@ -1,23 +1,26 @@
+// Program to print the area of triangle.
 
 public class AreaOfTriangle {
-	public static double area(double h, double b)
-	{
-		return (h*b/2);
-		
+
+	public static double areaTriangle(double height, double breadth) {
+		return (height * breadth / 2);
+
 	}
-public static void main(String[] args) {
-	
-//	double breadth=20.5;
-//	double height=110.5;
-//	
-//	
-//	double area=(breadth*height)/2;
-//	
-//	System.out.println("area is " +area);
-	
-	double h=10.56;
-	double b=4.45;
-	
-	System.out.println("area of triangle= " +area(h, b));
+
+	public static void main(String[] args) {
+
+		double height = 10.56;
+		double breadth = 14.45;
+
+		System.out.println("area of triangle= " + areaTriangle(height, breadth));
+	}
 }
-}
+
+/*
+ * double breadth = 20.5;
+ * double height = 10.5;
+ * 
+ * double area=(height * breadth) / 2;
+ * 
+ * System.out.println("area of triangle is = " + area);
+ */
