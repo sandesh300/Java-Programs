d7e0c045e6402ea9387b757b06b6c40b54e99414
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-11 16:56:36 +0530

    update the code

--- Java-Program-Important/SwapNumbersUsingMethod.java
+++ Java-Program-Important/SwapNumbersUsingMethod.java
@@ -1,21 +1,21 @@
-//swaps two numbers using method
+// Program to swaps two numbers using method
 
-public class SwapNumbersUsingMethod
-{
-    public static void swap(int a, int b)
-    {
-        a = (a & b) + (a | b);  //a=a+b
+public class SwapNumbersUsingMethod {
+    public static void swap(int a, int b) {
+        a = (a & b) + (a | b); // a = a + b
 
-        b = a + (~b) + 1;  //b=a-b
-        
-        a = a + (~b) + 1;  //a=a-b;
+        b = a + (~b) + 1; // b = a - b
 
-        System.out.println("After swapping : a= "+a+ "b= "+b);
+        a = a + (~b) + 1; // a = a - b
+
+        System.out.println("After swapping : a= " + a + "b= " + b);
     }
-    public static void main(String[] args)
-    {
-    int a = 10, b = 200;
-    swap(a, b);
+
+    public static void main(String[] args) {
+        int a = 10, b = 200;
+        swap(a, b);
 
     }
 }
+
+// output = After swapping : a= 200 b= 10
--- Java-Program-Important/vowels.java
+++ Java-Program-Important/vowels.java
@@ -1,25 +1,28 @@
+// Program to check character is vowel or consonant
 
 public class vowels {
-public static void main(String[] args) {
-	
-	char ch='A';
-	
-	 switch(ch)
-	 {
-	 case 'a':
-	 case 'e':
-	 case 'i':
-	 case 'o':
-	 case 'u':
-	 case 'A':
-	 case 'E':
-	 case 'I':
-	 case 'O':
-	 case 'U':
-		 System.out.println(ch+" it is vowel");
-		 break;
-		 default:System.out.println(ch+" It is consonant");
-	 }
-}
+	public static void main(String[] args) {
+
+		char ch = 'A';
+
+		switch (ch) {
+			case 'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+			case 'A':
+			case 'E':
+			case 'I':
+			case 'O':
+			case 'U':
+				System.out.println(ch + " it is vowel");
+				break;
+			default:
+				System.out.println(ch + " It is consonant");
+		}
+	}
 
 }
+
+// output = A it is vowel
