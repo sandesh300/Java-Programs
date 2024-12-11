6aebc63c460a6fa9717646765070aa2c5349dfb2
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-11 13:41:55 +0530

    ASCII value code

--- Java-Program-Important/ASCIIVAlue.java
+++ Java-Program-Important/ASCIIVAlue.java
@@ -1,10 +1,23 @@
+// Program to print the ASCII value of character.
 
 public class ASCIIVAlue {
-   public static void main(String[] args) {
-	
-	 for(int i=0; i<255; i++)
-	 {
-		 System.out.println("AScii value of " +(char)i+"------"+i);
-	 }
+	public static void main(String[] args) {
+
+		char ch = 'A';
+		int asciiValue = (int) ch;
+
+		System.out.println("ASCII value of " + ch + " is = " + asciiValue); // ASCII value of A is = 65
+
+	}
 }
-}
+
+
+/*
+to print ASCII values from 0 to 255
+
+for(int i=0; i<255; i++)
+ {
+
+ System.out.println("AScii value of " +(char)i+"-"+i);
+ }
+ */
