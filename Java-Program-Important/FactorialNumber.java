2af586d923a4ac59d7e767ed9608b40598cce84b
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-11 14:37:16 +0530

    code for factorial number

--- Java-Program-Important/FactorialNumber.java
+++ Java-Program-Important/FactorialNumber.java
@@ -1,15 +1,15 @@
-public class FactorialNumber
-{
+// Program to print factorial number.
+
+public class FactorialNumber {
     public static void main(String[] args) {
         int no = 5;
         int fact = 1;
 
-        for(int i=1; i<=no; i++)
-        {
+        for (int i = 1; i <= no; i++) {
             fact = fact * i;
         }
-        System.out.println("factorial no is "+fact );
+        System.out.println("factorial no is " + fact);
     }
 }
 
-//output = factorial of 5 is 120
+// output = factorial of 5 is 120
