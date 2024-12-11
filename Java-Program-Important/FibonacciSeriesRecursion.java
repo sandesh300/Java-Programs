f0a43030f3570ce2e69b9ae07d0cd1a6aa6e8ba2
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-11 15:20:54 +0530

    recursion fibonacci

--- Java-Program-Important/FibonacciSeriesRecursion.java
+++ Java-Program-Important/FibonacciSeriesRecursion.java
@@ -1,25 +1,26 @@
-//fibonacci series with recursion
+//Recursion program of fibonacci numbers.
 
 public class FibonacciSeriesRecursion {
 
-	static int n1=0,n2=1,n3=0;
-	static void printFibonacci(int count)
-	{
-		if(count>0)
-		{
-			n3=n1+n2;
-			n1=n2;
-			n2=n3;
-			System.out.println(" "+n3);
-          printFibonacci(count-1);
+	static int n1 = 0, n2 = 1, n3 = 0;
+
+	static void printFibonacci(int count) {
+		if (count > 0) {
+			n3 = n1 + n2;
+			n1 = n2;
+			n2 = n3;
+			System.out.println(" " + n3);
+			printFibonacci(count - 1);
 		}
 	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-      
-		int count=10;
-		System.out.println(n1+" "+n2);
-		printFibonacci(count-2);
+
+		int count = 10;
+		System.out.println(n1 + " " + n2);
+		printFibonacci(count - 2);
 	}
 
 }
+
+// output = 0 1 1 2 3 5 8 13 21 34
