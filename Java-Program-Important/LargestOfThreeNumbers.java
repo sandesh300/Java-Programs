04d1758c4556d2be060b6d7c758910e78b92e878
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-11 15:29:11 +0530

    code for the largest number

--- Java-Program-Important/LargestOfThreeNumbers.java
+++ Java-Program-Important/LargestOfThreeNumbers.java
@@ -1,22 +1,21 @@
-/// Find the larg number from the three numbers.
+// Program to print the largest number from the three numbers.
 
 public class LargestOfThreeNumbers {
 	public static void main(String[] args) {
 
 		int n1 = 10;
 		int n2 = 15;
-        int n3 = 44;
-       
-        int temp, largest;
-        
-        temp = n1 > n2 ? n1 : n2;
-        
-        largest = temp > n3 ? temp : n3;
-        
-		System.out.println("The largest number is = " + largest);
+		int n3 = 44;
+
+		int temp, largest;
+
+		temp = n1 > n2 ? n1 : n2;
+
+		largest = temp > n3 ? temp : n3;
+
+		System.out.println("The largest number is: " + largest);
 
 	}
 }
 
-
-// output = The largest number is = 44
+// output = The largest number is: 44
