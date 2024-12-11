7e52fc1c7f9306f09b51eb4d1131bf3b7197c14b
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-11 15:55:56 +0530

    Prime number code

--- Java-Program-Important/PrimeNumber.java
+++ Java-Program-Important/PrimeNumber.java
@@ -1,63 +1,51 @@
+// Program to check number is prime or not.
 
-//to check number is prime or not
-//7=1 & 7....no-1=2,3,4,5,6...(logic part)
+public class PrimeNumber {
+	public static void main(String[] args) {
+		int no = 9;
+		int temp = 0;
 
-public class PrimeNumber 
-{	
-	public static void main(String[] args)
-	{
-    int no=9;      	
-    int temp=0;
-    
-    for(int i=2;i<no-1;i++)
-    {
-   	 if(no%i==0)
-   	 {
-   		 temp=temp+1;
-   	 }
-    }
-	
-    if(temp==0)
-    {
-   	 System.out.println(no+" Is Prime");
-    }
-    else {
-   	 System.out.println(no+" Is not prime ");
-    }
-	
+		for (int i = 2; i < no - 1; i++) {
+			if (no % i == 0) {
+				temp = temp + 1;
+			}
+		}
+
+		if (temp == 0) {
+			System.out.println("Prime number");
+		} else {
+			System.out.println("Not prime number");
+		}
+
+	}
+
+}
+
+// output = Prime number
+
+/* ------------------------------------------------------------------------------------------------------------------------------
+// Program to print prime numbers between 1 to 100
+
+	public static void main(String[] args) {
+		for (int no = 1; no < 100; no++)
+
+		{
+			int temp = 0;
+			for (int i = 2; i < no - 1; i++) {
+				if (no % i == 0) {
+					temp = temp + 1;
+				}
+			}
+
+			if (temp == 0) {
+				System.out.println(no);
+			} else {
+				temp = 0;
+			}
+		}
 	}
 }
 
-	
-	//------------------------------------------------------------------------------------------------------------------------------
-     //to print prime numbers between 1 to 100....
-	///we need 2 for loop 1 for print no and 1 for check condition
+// output = 1 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 
 
-	public static void main(String[] args)
-	{	
-    		for (int no=1;no<100;no++)
-    			
-    		{
-    			int temp=0;
-    			for(int i=2;i<no-1;i++)
-    			{
-    				if(no%i==0)
-    				{
-    					temp=temp+1;
-    				}
-    			}
-    			
-    			if(temp==0)
-    			{
-    				System.out.println(no);
-    			}
-    			else
-    			{
-    				temp=0;
-    			}
-    		}
-	}
-}
-	
-
-
+*/
