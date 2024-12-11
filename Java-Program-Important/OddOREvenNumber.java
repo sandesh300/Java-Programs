c1c3323f6142d4523a99644f385a7661e9a6be87
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-11 15:31:45 +0530

    odd or even number code

--- Java-Program-Important/OddOREvenNumber.java
+++ Java-Program-Important/OddOREvenNumber.java
@@ -1,15 +1,15 @@
+// Program to check the number is even or odd.
 
 public class OddOREvenNumber {
-public static void main(String[] args) {
-	int num=57;
-	
-	if(num % 2 == 0 )
-	{
-		System.out.println("even number");
+	public static void main(String[] args) {
+		int num = 57;
+
+		if (num % 2 == 0) {
+			System.out.println("even number");
+		} else {
+			System.out.println("odd number");
+		}
 	}
-	else
-	{
-		System.out.println("odd number");
-	}
-}  
 }
+
+// output = odd number
