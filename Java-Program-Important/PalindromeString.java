a9171d7240dc3bab85b53118ef58a97f0bb56b64
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-11 15:49:24 +0530

    Palindrome string code

--- Java-Program-Important/PalindromeString.java
+++ Java-Program-Important/PalindromeString.java
@@ -1,25 +1,22 @@
+// Program to check given string is palindrome or not.
 
 public class PalindromeString {
- public static void main(String[] args) {
-	 
-	String str = "madam";
-	 
-   	String reverse="";
-	
-   	
-	for(int i=str.length()-1; i>=0; i--)
-	{
-		reverse=reverse + str.charAt(i);
-	}
-	
-	
-	if(str.equals(reverse))
-	{
-		System.out.println("String is palindrome");
-	}
-	else
-	{
-		System.out.println("String is not palindrome");
+	public static void main(String[] args) {
+
+		String str = "madam";
+
+		String reverse = "";
+
+		for (int i = str.length() - 1; i >= 0; i--) {
+			reverse = reverse + str.charAt(i);
+		}
+
+		if (str.equals(reverse)) {
+			System.out.println("String is palindrome");
+		} else {
+			System.out.println("String is not palindrome");
+		}
 	}
 }
-}
+
+// output = String is palindrome
