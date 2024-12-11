9695de2fb284bb70120156983e3a3908d5d5c217
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-11 14:45:29 +0530

    factorial recursion method

--- Java-Program-Important/FactorialNumRecursion.java
+++ Java-Program-Important/FactorialNumRecursion.java
@@ -1,20 +1,19 @@
+// Recursion program of factorial number.
+
 public class FactorialNumRecursion {
-    static int factorial(int no)
-    {
-        if(no==0)
-        {
+
+    static int factorial(int no) {
+
+        if (no == 0) {
             return 1;
-        }
-        else
-        {
-            return (no*factorial(no-1));
+        } else {
+            return (no * factorial(no - 1));
         }
     }
-    
+
     public static void main(String[] args) {
         int no = 5;
-        int fact= 1;
-        fact = factorial(no);
-           System.out.println("factorial no "+ fact);
+        int fact = factorial(no);
+        System.out.println("factorial no: " + fact); // factorial no: 128
     }
 }
