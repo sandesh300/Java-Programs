a3df816d52010046c1856aa5dfc5a290dabd4d61
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-12 14:53:56 +0530

    update code for sort array by sort()

--- JAVA-Array-Programs/SortArraySortMethod.java
+++ JAVA-Array-Programs/SortArraySortMethod.java
@@ -1,13 +1,19 @@
 import java.util.Arrays;
-public class SortArraySortMethod {
-public static void main(String[] args) {
-	
-	int arr[]=new int[] {11,22,99,56,54,34,4};
 
-   System.out.printf ("originl array = %s ",Arrays.toString(arr));
-   
-   Arrays.sort(arr);
-   
-   System.out.printf("\n Sorted arrray  =  %s",Arrays.toString(arr));
+public class SortArraySortMethod {
+   public static void main(String[] args) {
+
+      int arr[] = { 11, 22, 99, 56, 54, 34, 4 };
+
+      System.out.printf("originl array = %s ", Arrays.toString(arr));
+
+      Arrays.sort(arr);
+
+      System.out.printf("\n Sorted arrray  =  %s", Arrays.toString(arr));
+   }
 }
-}
+
+/*
+ originl array = [11, 22, 99, 56, 54, 34, 4] 
+ Sorted arrray  =  [4, 11, 22, 34, 54, 56, 99]
+ */
