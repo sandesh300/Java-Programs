caecb96a62873f16d4d0df9f01e2a11e6f8c38a2
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-12 14:52:43 +0530

    update code for  reverse array

--- JAVA-Array-Programs/ReverseArray.java
+++ JAVA-Array-Programs/ReverseArray.java
@@ -1,28 +1,29 @@
+// Program to reverse the array.
 
 public class ReverseArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        
-		int arr[]=new int[] {1,2,3,4,5};
-		
+
+		int arr[] = { 1, 2, 3, 4, 5 };
+
 		System.out.println("displaying an orginal array= ");
-		for(int i=0; i<arr.length;i++)
-		{
-			System.out.print(arr[i]+" ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
 		}
 		System.out.println();
-		
-		
-		
-		
-		
-		
+
 		System.out.println("\ndisplaying an reverse array= ");
-		for(int i=arr.length-1; i>=0; i--)
-		{
-			System.out.print(arr[i]+" ");
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr[i] + " ");
 		}
 	}
 
 }
+
+/*
+displaying an orginal array= 
+1 2 3 4 5 
+
+displaying an reverse array= 
+5 4 3 2 1 
+ */
