03a36f372ebdaa6f328c23b8b24707554aa6856a
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-12 14:51:08 +0530

    update code of largest array

--- JAVA-Array-Programs/LargestArray.java
+++ JAVA-Array-Programs/LargestArray.java
@@ -1,15 +1,15 @@
+// Program to print the maximum element from the array.
 
 public class LargestArray {
-public static void main(String[] args) {
-	
-	int arr[]=new int[] {10,23,45,56,77};
-	int max=arr[0];
-	
-	for(int i=1; i<arr.length; i++)
-	{
-		if(arr[i]>max)  // > sign for max array,  if you change this to < sign it print small array
-			max=arr[i];
+	public static void main(String[] args) {
+
+		int arr[] = new int[] { 10, 23, 45, 56, 77 };
+		int max = arr[0];
+
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > max) // > sign for max array, if you change this to < sign it print small element
+				max = arr[i];
+		}
+		System.out.println("largest: " + max); // largest: 77
 	}
-	System.out.println("largest "+max);
-}
 }
