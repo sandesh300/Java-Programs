74b1dd9a1d2c75f35ae725f3b2bdca395d491574
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-12 14:50:44 +0530

    update code of  frequency element

--- JAVA-Array-Programs/FreqeuencyElement.java
+++ JAVA-Array-Programs/FreqeuencyElement.java
@@ -1,35 +1,41 @@
 // write the program to find frequency of elements.
 
 import java.util.Arrays;
-public class FreqeuencyElement 
-{	
- public static void main(String[] args) {
-	int[] arr = {1,3,5,7,9,1,3,3,4,9};
-	int n=arr.length;
-   countfreq(arr, n);
+
+public class FreqeuencyElement {
+	public static void main(String[] args) {
+		int[] arr = { 1, 3, 5, 7, 9, 1, 3, 3, 4, 9 };
+		int n = arr.length;
+		countfreq(arr, n);
+	}
+
+	public static void countfreq(int[] arr, int n) {
+		boolean[] visited = new boolean[n];
+		Arrays.fill(visited, false);
+
+		for (int i = 0; i < n; i++) {
+			if (visited[i] == true)
+				continue;
+
+			int count = 1;
+			for (int j = i + 1; j < n; j++) {
+				if (arr[i] == arr[j]) {
+					visited[j] = true;
+					count++;
+
+				}
+			}
+			System.out.println(arr[i] + " " + count);
+		}
+	}
 }
- public static void countfreq(int[] arr, int n)
- {
-	 boolean[] visited = new boolean[n];
-	 Arrays.fill(visited, false);
-	 
-	 for(int i=0 ; i<n; i++)
-	 {
-		 if(visited[i] == true)
-			 continue;
-		 
-		 int count = 1;
-		 for(int j=i+1; j<n; j++)
-		 {
-			 if(arr[i] == arr[j])
-			 {
-				 visited[j] = true;
-				 count++;
-				 
-			 }
-		 }
-    System.out.println(arr[i] +" "+ count);
-	 }
- }
-}
-  
+
+/*
+output =
+1 2
+3 3
+5 1
+7 1
+9 2
+4 1
+ */
