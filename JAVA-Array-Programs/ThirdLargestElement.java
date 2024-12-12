a13b5776f66fa71d0319e091b61736070aa634a4
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-12 14:54:34 +0530

    update code for third largest array

--- JAVA-Array-Programs/ThirdLargestElement.java
+++ JAVA-Array-Programs/ThirdLargestElement.java
@@ -1,30 +1,25 @@
+// Program to print the third largest number from array.
 
 public class ThirdLargestElement {
-	
-	public static int getThirdLargest(int a[], int total)
-	{
+
+	public static int getThirdLargest(int arr[]) {
 		int temp;
-		for(int i=0; i<total; i++)
-		{
-			for(int j=i+1; j<total; j++)
-			{
-				if(a[i]>a[j])
-				{
-					temp=a[i];
-					a[i]=a[j];
-					a[j]=temp;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
 				}
 			}
 		}
-		return a[total-3];   // if we write [total-2] then it will print second largest element of an array 
+		return arr[arr.length - 3]; // if we write [total-2] then it will print second largest element of an array
 	}
-										
-  public static void main(String[] args) 
-  {
-  int arr[] = {11,25,65,99,36,24};	
 
-	System.out.println("third largest number = "+getThirdLargest(arr, 6));
-	//getThirdLargest(a, 6);
-	
-}
+	public static void main(String[] args) {
+		int arr[] = { 11, 25, 65, 99, 36, 24 };
+
+		System.out.println("third largest number: " + getThirdLargest(arr)); // third largest number: 36
+
+	}
 }
