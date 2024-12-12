2fe5d572dd1641965d37da57669751a697e857ec
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-12 14:50:18 +0530

    update code of duplicate array

--- JAVA-Array-Programs/DuplicateArray.java
+++ JAVA-Array-Programs/DuplicateArray.java
@@ -1,11 +1,11 @@
-// write a program to display the duplicate arrays.
+// Program to print the duplicate array elements.
 
 public class DuplicateArray {
 	public static void main(String[] args) {
 
 		int arr[] = { 1, 2, 3, 4, 2, 7, 8, 8, 7, 3 };
 
-		System.out.println("Displaying duplicate array= ");
+		System.out.println("Displaying duplicate array elemnts= ");
 
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = i + 1; j < arr.length; j++) {
@@ -17,4 +17,4 @@ public class DuplicateArray {
 	}
 }
 
-// output = Displaying duplicate array= 2, 3, 7, 8
+// output = Displaying duplicate array elemnts= 2, 3, 7, 8
