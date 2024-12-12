a9a62bf89096bb87f37c00956327c881068a7d79
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-12 14:50:30 +0530

    update code of  even and odd

--- JAVA-Array-Programs/EvenOddNumber.java
+++ JAVA-Array-Programs/EvenOddNumber.java
@@ -1,30 +1,23 @@
-// write the program to print even and odd number from the array.
- 
+// Program to print even and odd numbers from the array.
+
 public class EvenOddNumber {
-public static void main(String[] args) {
-	
-	int arr[]= {1,2,3,4,5,6};
-	
-	System.out.println("Even Numbers = ");
-	for(int i=0; i<arr.length; i++)
-	{
-		if(arr[i] % 2 == 0)
-		{
-			System.out.println(arr[i]);     // Even Numbers = 2, 4, 6
+	public static void main(String[] args) {
+
+		int arr[] = { 1, 2, 3, 4, 5, 6 };
+
+		System.out.println("Even Numbers = ");
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] % 2 == 0) {
+				System.out.println(arr[i]); // Even Numbers = 2, 4, 6
+			}
 		}
+
+		System.out.println("Odd Numbers = ");
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] % 2 != 0) {
+				System.out.println(arr[i]); // Odd Numbers = 1, 3, 5
+			}
+		}
+
 	}
-
-
-	
-	System.out.println("Odd Numbers = ");
-	for(int i=0; i<arr.length; i++)
-	{
-		if(arr[i] % 2 != 0)
-		{
-			System.out.println(arr[i]);    // Odd Numbers = 1, 3, 5
-		}
-	}	
-
-	
-}
 }
