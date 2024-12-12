95ee469d9cfd1a877d29eb78b0eef4578fc02a03
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-12 14:53:38 +0530

    update code for smallest array

--- JAVA-Array-Programs/SmallestArray.java
+++ JAVA-Array-Programs/SmallestArray.java
@@ -1,19 +1,16 @@
+// Program to print the minimum element from the array.
 
 public class SmallestArray {
 
-public static void main(String[] args) {
-	
-	int arr[]=new int[] {10,23,45,56,77};
-	int min=arr[0];
-	
-	for(int i=1; i<arr.length; i++)
-	{
-		if(arr[i]< min)  // > sign for max array if you change this to < sign it print small array
-			min=arr[i];
+	public static void main(String[] args) {
+
+		int arr[] = { 100, 23, 11, 45, 56, 77 };
+		int min = arr[0];
+
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] < min) // > sign for max array if you change this to < sign it print small array
+				min = arr[i];
+		}
+		System.out.println("smallest: " + min);  // smallest: 11
 	}
-	System.out.println("largest "+min);
 }
-}
-
-
-
