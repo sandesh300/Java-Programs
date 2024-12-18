3c1a63aa31a5cc0dd1e0f747b0e4763abeb24d49
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-18 20:48:16 +0530

    update cod eof subset of string

--- JAVA-String-Programs/FindSubsetOfString.java
+++ JAVA-String-Programs/FindSubsetOfString.java
@@ -1,24 +1,61 @@
+// Program to print the substring of string.
 
 public class FindSubsetOfString {
-public static void main(String[] args) {
-	String str="Sandesh ";
-	int temp=0;
-	int len=str.length();
-	
-	String[] arr=new String[len*(len+1)/2];
+	public static void main(String[] args) {
+		String str = "Sandesh";
+		int temp = 0;
+		int len = str.length();
 
-      for(int i=0; i<len; i++)
-      {
-    	  for(int j=i; j<len; j++)
-    	  {
-    		  arr[temp]=str.substring(i, j+1);
-    		  temp++;
-    	  }
-      }
-      System.out.println("The Subset of Strings= ");
-      for(int i=0; i<arr.length; i++)
-      {
-    	  System.out.println(arr[i]);
-      }
+		String[] arr = new String[len * (len + 1) / 2];
+
+		for (int i = 0; i < len; i++) {
+			for (int j = i; j < len; j++) {
+				arr[temp] = str.substring(i, j + 1);
+				temp++;
+			}
+		}
+		System.out.println("The Subset of String: ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
 }
-}
+
+/*
+ The Subset of Strings= 
+S       
+Sa      
+San     
+Sand    
+Sande   
+Sandes  
+Sandesh 
+Sandesh 
+a       
+an
+and
+ande
+andes
+andesh
+andesh
+n
+nd
+nde
+ndes
+ndesh
+ndesh
+d
+de
+des
+desh
+desh
+e
+es
+esh
+esh
+s
+sh
+sh
+h
+h
+ */
