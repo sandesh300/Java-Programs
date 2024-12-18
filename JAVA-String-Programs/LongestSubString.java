f3a9ec8d0c494074c42dbf8060db099b1c3e6d72
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-18 21:44:26 +0530

    update code of longest sub string code

--- JAVA-String-Programs/LongestSubString.java
+++ JAVA-String-Programs/LongestSubString.java
@@ -1,14 +1,15 @@
 // Q - Length of Longest Substring without any Repeating Character
+/* 
+Example 1:
+Input: s = ”abcabcbb”
+Output: 3
+Explanation: The answer is abc with length of 3.
 
-// Example 1:
-// Input: s = ”abcabcbb”
-// Output: 3
-// Explanation: The answer is abc with length of 3.
-
-// Example 2:
-// Input: s = ”bbbbb”
-// Output: 1
-// Explanation: The answer is b with length of 1 units.
+Example 2:
+Input: s = ”bbbbb”
+Output: 1
+Explanation: The answer is b with length of 1 units.
+*/
 
 import java.util.*;
 
@@ -18,12 +19,12 @@ public class LongestSubString {
         if (str.length() == 0)
             return 0;
         int maxans = Integer.MIN_VALUE;
-        for (int i = 0; i < str.length(); i++) // outer loop for traversing the string
+        for (int i = 0; i < str.length(); i++) 
         {
             Set<Character> se = new HashSet<>();
-            for (int j = i; j < str.length(); j++) // nested loop for getting different string starting with str[i]
+            for (int j = i; j < str.length(); j++) 
             {
-                if (se.contains(str.charAt(j))) // if element if found so mark it as ans and break from the loop
+                if (se.contains(str.charAt(j))) // if element found so mark it as ans and break from the loop
                 {
                     maxans = Math.max(maxans, j - i);
                     break;
@@ -35,9 +36,12 @@ public class LongestSubString {
     }
 
     public static void main(String args[]) {
-        String str = "takeUforward";
-        System.out.println(
-                "The length of the longest substring without repeating characters is " + longestSubstring(str));
+        String str = "abcabcbb";
+        System.out.println("The length of the longest substring without repeating characters is: " + longestSubstring(str));
 
     }
 }
+
+/*
+The length of the longest substring without repeating characters is: 3
+ */
