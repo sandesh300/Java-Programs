af5c127aba5f6294f0d510fe17839e3fb60402e8
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-19 17:06:41 +0530

    update code of linear seach algorithm

--- JAVA-Searching&Sorting/LinearSearchExample.java
+++ JAVA-Searching&Sorting/LinearSearchExample.java
@@ -1,4 +1,4 @@
-// Linear-Search program.
+// Linear-Search program. Time Complexity = (0)n
 
 public class LinearSearchExample {
 
@@ -12,14 +12,14 @@ public class LinearSearchExample {
 	}
 
 	public static void main(String[] args) {
-		int a1[] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+		int arr[] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
 
 		int key = 50;
 
-		System.out.println(key + " is found at " + LinearSearch(a1, key));
+		System.out.println(key + " is found at index:" + LinearSearch(arr, key));
 
 	}
 
 }
 
-// output - 50 is found at 4
+// output - 50 is found at index: 4
