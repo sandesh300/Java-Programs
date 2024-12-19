c24b851b5289613ab6c615e08059dce7b5bb06d9
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-19 16:12:33 +0530

    update code of star pattern of Left pascal

--- JAVA-Star-Pattern/LeftpascalsTrianglePattern.java
+++ JAVA-Star-Pattern/LeftpascalsTrianglePattern.java
@@ -1,31 +1,39 @@
 
 public class LeftpascalsTrianglePattern {
-public static void main(String[] args) {
-	for(int i=1; i<5; i++)
-	{
-		for(int k=4; k>i; k--)
-		{
-			System.out.print(" ");
+	public static void main(String[] args) {
+		for (int i = 1; i < 5; i++) {	
+			for (int k = 4; k > i; k--) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
-		for(int j=1; j<=i; j++)
-		{
-			System.out.print("*");
+
+		for (int i = 3; i >= 1; i--) {
+			for (int k = 4; k > i; k--) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
-		System.out.println();
+
 	}
-	
-	for(int i=3; i>=1; i--)
-	{
-		for(int k=4; k>i; k--)
-		{
-			System.out.print(" ");
-		}
-		for(int j=1; j<=i; j++)
-		{
-			System.out.print("*");
-		}
-		System.out.println();
-	}
-	
 }
-}
+
+/*
+
+
+   *
+  **
+ ***
+****
+ ***
+  **
+   *
+
+   
+*/
