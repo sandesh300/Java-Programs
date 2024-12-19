57971dd536ff6404053ee5874ea24d5f42d0c641
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-19 16:12:43 +0530

    update code of star pattern of left star

--- JAVA-Star-Pattern/leftStarPattern.java
+++ JAVA-Star-Pattern/leftStarPattern.java
@@ -1,19 +1,33 @@
-import java.util.Arrays;
 
 public class leftStarPattern {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		int arr[] = new int[3];
-		arr[0]=11;
-		arr[1]=22;
-		arr[2]=33;
-		 System.out.println(Arrays.toString(arr));
-		for(int i=0; i<arr.length; i++)
-		{
-			System.out.println(arr[i]+" ");
+		// Loop for each row
+		for (int i = 1; i <= 5; i++) {
+			// Print spaces for alignment
+			for (int j = 1; j <= 5 - i; j++) {
+				System.out.print(" ");
+			}
+			// Print stars for the current row
+			for (int k = 1; k <= i; k++) {
+				System.out.print("*");
+			}
+			// Move to the next line
+			System.out.println();
 		}
 	}
 
 }
+
+/*
+
+
+    *
+   **
+  ***
+ ****
+*****
+
+
+ */
