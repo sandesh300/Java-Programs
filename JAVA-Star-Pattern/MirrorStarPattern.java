0756e1a2e4682a196417566d9ab9e5a25197a854
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-19 16:12:52 +0530

    update code of star pattern of mirror star

--- JAVA-Star-Pattern/MirrorStarPattern.java
+++ JAVA-Star-Pattern/MirrorStarPattern.java
@@ -1,19 +1,28 @@
 
 public class MirrorStarPattern {
-public static void main(String[] args) {
-	for(int i=4; i>=1; i--)
-	{
-		for(int k=4; k>i; k--)
-		{
-			System.out.print(" ");
-		}
-		
-			for(int j=1; j<=i; j++)
-			{
+	public static void main(String[] args) {
+		for (int i = 4; i >= 1; i--) {
+			for (int k = 4; k > i; k--) {
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
 			}
-		
-	System.out.println();
+
+			System.out.println();
+		}
+	}
 }
-}
-}
+
+/*
+
+
+****
+ ***
+  **
+   *
+   * 
+
+
+ */
