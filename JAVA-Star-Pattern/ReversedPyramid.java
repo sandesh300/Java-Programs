56075fb54e48b2de40c17fdb8431910f54457426
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-19 16:13:23 +0530

    update code of star pattern of reversed pyramid

--- JAVA-Star-Pattern/ReversedPyramid.java
+++ JAVA-Star-Pattern/ReversedPyramid.java
@@ -1,17 +1,26 @@
 
 public class ReversedPyramid {
-public static void main(String[] args) {
-	
-	for(int i=1; i<5; i++)
-	{
-		for(int k=1; k<i; k++) {
-			System.out.print(" ");
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) {
+			for (int k = 1; k < i; k++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= 5 - i; j++) {
+				System.out.print("*" + " ");
+			}
+			System.out.println();
+		}
 	}
-	for(int j=1; j<5-i; j++)
-	{
-		System.out.print("*"+ " ");
-	}
-	System.out.println();
 }
-}
-} 
+
+/*
+ 
+
+* * * * 
+ * * * 
+  * * 
+   * 
+
+
+ */
