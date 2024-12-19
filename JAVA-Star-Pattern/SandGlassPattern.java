039dd65621de20b5c46d4c27020236a3d3efa0d0
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-19 16:13:46 +0530

    update code of star pattern of sand glass pattern

--- JAVA-Star-Pattern/SandGlassPattern.java
+++ JAVA-Star-Pattern/SandGlassPattern.java
@@ -1,33 +1,41 @@
 
-public class SandGlassPattern
-{
-public static void main(String[] args) {
-	
-	for(int i=1; i<=4; i++)
-	{
-		for(int k=1; k<i; k++)
-		{
-			System.out.print(" ");
+public class SandGlassPattern {
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 4; i++) {
+			for (int k = 1; k < i; k++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= 5 - i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
 		}
-		for(int j=1; j<=5-i; j++)
-		{
-			System.out.print("* ");
+
+		for (int i = 1; i <= 4; i++) {
+			for (int k = 3; k >= i; k--) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
 		}
-		System.out.println();
 	}
+}
 
 
-for(int i=1; i<=4; i++)
-{
-	for(int k=3; k>=i; k--)
-	{
-		System.out.print(" ");
-	}
-	for(int j=1; j<=i; j++)
-	{
-		System.out.print("* ");
-	}
-	System.out.println();
-}
-}
-}
+/*
+ 
+
+* * * * 
+ * * *  
+  * *   
+   *    
+   *    
+  * *   
+ * * *  
+* * * * 
+
+
+ */
