5de8fe141065f3a8b84f56b6166fdb4dcb6da19b
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-19 17:39:07 +0530

    update code of  selection sort algorithm

--- JAVA-Searching&Sorting/SelectionSortExample.java
+++ JAVA-Searching&Sorting/SelectionSortExample.java
@@ -1,53 +1,42 @@
+// Selection Sort Algorithm. Time Complexity = 0(n^2)
 
 public class SelectionSortExample {
 
-	public static void SelectionSort(int arr[])
-	{
-		for(int i=0; i<arr.length; i++)
-		{
-			int index=i;
-			for(int j=i+1; j<arr.length; j++)
-			{
-				if(arr[j]<arr[index])
-				{
-					index=j;
+	public static void SelectionSort(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			int index = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] < arr[index]) {	
+					index = j;
 				}
 			}
-			int smallnumber=arr[index];
-			arr[index]=arr[i];
-			arr[i]=smallnumber;
+			int smallnumber = arr[index];
+			arr[index] = arr[i];
+			arr[i] = smallnumber;
 		}
 	}
-	
-	
-	public static void main(String[] args) 
-	{
-		int arr1[]= {9,14,3,2,43,11,58,22};
+
+	public static void main(String[] args) {
+		int arr[] = { 9, 14, 3, 2, 43, 11, 58, 22 };
 		System.out.println("array before selction sort ");
-		for(int i=0; i<arr1.length; i++)
-		{
-			System.out.print(arr1[i]+"  ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + "  ");
 		}
 		System.out.println();
-		
-		
-		SelectionSort(arr1);
+
+		SelectionSort(arr);
 		System.out.println("array After selction sort ");
-		for(int i=0; i<arr1.length; i++)
-		{
-			System.out.print(arr1[i]+"  ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + "  ");
 		}
 		System.out.println();
-		
-		
-		
-		// we can use for each
-//		System.out.println("array After selction sort ");
-//		for(int p:arr1)
-//		{
-//			System.out.print(p+"  ");
-//		}
-		
-		
+
 	}
 }
+
+/*
+array before selction sort 
+9  14  3  2  43  11  58  22  
+array After selction sort    
+2  3  9  11  14  22  43  58 
+*/
