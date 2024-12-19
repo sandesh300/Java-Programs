91335b1453005a97a85e83a6ae0bc1cb52e7c1c6
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-19 16:13:02 +0530

    update code of star pattern of pascal triangle

--- JAVA-Star-Pattern/PascalsTriangle.java
+++ JAVA-Star-Pattern/PascalsTriangle.java
@@ -1,38 +1,37 @@
-//pascals triangle= 
-//*
-//* *
-//* * *
-//* * * *
-//* * * 
-//* *
-//*
-
-
-
+// pascals triangle 
 
 public class PascalsTriangle {
- public static void main(String[] args) {
-	
-	 //right triangle
-	 for(int i=1; i<=4; i++)
-	 {
-		 for(int j=1; j<=i; j++)
-		 {
-			 System.out.print("*  ");
-		 }
-		 System.out.println();
-	 }
- 
- 
- //downward triangle
- for(int i=4; i>=1; i--)
- {
-	 for(int j=1; j<=i-1; j++)
-	 {
-		 System.out.print("*  ");
-	 }
-	 System.out.println();
- }
- 
+	public static void main(String[] args) {
+
+		// right triangle
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*  ");
+			}
+			System.out.println();
+		}
+
+		// downward triangle
+		for (int i = 4; i >= 1; i--) {
+			for (int j = 1; j <= i - 1; j++) {
+				System.out.print("*  ");
+			}
+			System.out.println();
+		}
+
+	}
 }
-}
+
+/*
+
+
+*  
+*  *        
+*  *  *     
+*  *  *  *  
+*  *  *     
+*  *        
+* 
+
+
+ */
