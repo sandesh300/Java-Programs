1ad286169ee27e987e4e8880ef0d52474d4b5e01
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-24 14:55:50 +0530

    update code of triangle matrix

--- JAVA-Matrix-Programs/TriangleMatrix.java
+++ JAVA-Matrix-Programs/TriangleMatrix.java
@@ -1,37 +1,37 @@
+// Lower Triangular matrix
 
 public class TriangleMatrix {
-public static void main(String[] args) {
-	int rows, cols;
-	
-	int a[][]= {{1,1,1},{1,1,1},{1,1,1}};
-	
-	rows=a.length;
-	cols=a[0].length;
-	
-	if(rows != cols)
-	{
-		System.out.println("MAtrices should be square");
-	}
-	else
-	{
-		System.out.println("Lower Triangular matrix ");
-		for(int i=0; i<rows; i++)
-		{
-			for(int j=0; j<cols; j++)
-			{
-				if(i<j)   //if you change < to > greater than sign it will print upper triangular matrix
-					
-					System.out.print("0 ");
-				else
-					System.out.print(a[i][j]+ " ");
-			}
+	public static void main(String[] args) {
+		int rows, cols;
+
+		int a[][] = { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
+
+		rows = a.length;
+		cols = a[0].length;
+
+		if (rows != cols) {
+			System.out.println("MAtrices should be square");
+		} else {
+			System.out.println("Lower Triangular matrix ");
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < cols; j++) {
+					if (i < j) // if you change < to > greater than sign it will print upper triangular matrix
+
+						System.out.print("0 ");
+					else
+						System.out.print(a[i][j] + " ");
+				}
 				System.out.println();
-			
+
+			}
 		}
+
 	}
-	
-	
-	
-	
 }
-}
+
+/*
+Lower Triangular matrix
+1 0 0
+1 1 0
+1 1 1
+ */
