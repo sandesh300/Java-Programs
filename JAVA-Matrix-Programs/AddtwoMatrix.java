f606eb2ea7cd888291621e66fe35dce5dea64f17
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-24 14:54:24 +0530

    update code of add two matrix

--- JAVA-Matrix-Programs/AddtwoMatrix.java
+++ JAVA-Matrix-Programs/AddtwoMatrix.java
@@ -1,21 +1,29 @@
+// Program to add two matrix in java.
 
 public class AddtwoMatrix {
-public static void main(String[] args) {
-	
-	int a[][]={{10,12,13},{4,5,11},{6,8,5}};
-	int b[][]= {{6,1,4},{8,5,2},{9,5,4}};
-	
-	int c[][]=new int[3][3];
-	
-	for(int i=0; i<3; i++)
-	{
-		for(int j=0; j<3; j++)
-		{
-			c[i][j]=a[i][j] * b[i][j]; // if you set " - " sign then subtraction operation occurs,,,,,, if you set " * " sign then multiplication operation occurs
-			
-			System.out.print(c[i][j] + " ");
+	public static void main(String[] args) {
+
+		int a[][] = { { 10, 12, 13 }, { 4, 5, 11 }, { 6, 8, 5 } };
+		int b[][] = { { 6, 1, 4 }, { 8, 5, 2 }, { 9, 5, 4 } };
+
+		int c[][] = new int[3][3];
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				c[i][j] = a[i][j] * b[i][j]; // if you set " - " sign then subtraction operation, if you set
+												// " * " sign then multiplication operation occurs
+
+				System.out.print(c[i][j] + " ");
+			}
+
+			System.out.println();
 		}
-	
-	System.out.println();
-}}
+	}
 }
+
+/*
+output - 
+60 12 52 
+32 25 22 
+54 40 20 
+ */
