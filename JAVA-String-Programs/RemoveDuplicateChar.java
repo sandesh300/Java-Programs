b2e3e4b2decd9498b41466553ac1bf415e7639ac
sandesh300 <bhujbalsandesh52@gmail.com>
2024-12-29 14:05:57 +0530

    update the code

--- JAVA-String-Programs/RemoveDuplicateChar.java
+++ JAVA-String-Programs/RemoveDuplicateChar.java
@@ -11,8 +11,26 @@ Output: "cbad"
 */
 
 public class RemoveDuplicateChar {
-    
+    public static String removeDuplicateLetter(String str) {
+        String ans = "";
+        for (int i = 0; i < str.length(); i++) {
+            int j = 0;
+            for (j = 0; j < i; j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    break;
+                }
+            }
+            if (i == j) {
+                ans = ans + str.charAt(i);
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        
+    String str = "bcabc";
+    System.out.println("The String after removing all the duplicate characters is: " + removeDuplicateLetter(str)); 
     }
 }
+
+// The String after removing all the duplicate characters is: bca
