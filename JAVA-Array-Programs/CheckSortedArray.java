3056050f51682293d6bcfb46a31f5c811ef0286e
sandesh300 <bhujbalsandesh52@gmail.com>
2025-01-01 23:15:49 +0530

    check sorted array problem statement

---
+++ JAVA-Array-Programs/CheckSortedArray.java
@@ -0,0 +1,17 @@
+/*
+Check if an Array is Sorted.
+Problem Statement: Given an array of size n, write a program to check if the given array is sorted in (ascending / Increasing / Non-decreasing) order or not. If the array is sorted then return True, Else return False.
+
+Example 1:
+Input: N = 5, array[] = {1,2,3,4,5}
+Output: True.
+
+Example 2:
+Input: N = 5, array[] = {5,4,6,7,8}
+Output: False.
+ */
+
+
+public class CheckSortedArray {
+    
+}
