d24aa6742ab4e4678b6ec992a01631e2de4c7b81
sandesh300 <bhujbalsandesh52@gmail.com>
2025-01-21 20:18:51 +0530

    add code search elements

--- JAVA-Array-Programs/SearchElement.java
+++ JAVA-Array-Programs/SearchElement.java
@@ -1,5 +1,21 @@
+// Program to search an element in an array
+
 public class SearchElement {
     public static void main(String[] args) {
+        int[] arr = { 5, 10, 20, 40, 80, 100 };
 
+        int searchElement = 20;
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == searchElement) {
+                found = true;
+                break;
+            }
+        }
+        if (found)
+            System.out.println("Element found in the array");
+        else
+            System.out.println("Element not found in the array");
     }
 }
